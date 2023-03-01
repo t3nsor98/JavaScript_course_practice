500ca3a4b5809fbd0b442322ad551f3b737a1959
Digbijaya Lenka <digbijayalenka@gmail.com>
2023-03-01 23:56:23 +0530

    some modifications in counter app
    added counter app on front HTML

--- counter/script.js
+++ counter/script.js
@@ -7,14 +7,26 @@ let count = 0;
 decrease.addEventListener("click", () => {
     count--;
     counter.textContent = count;
+    if (count > 0) {
+        counter.style.color = "green";
+    } else if (count < 0) {
+        counter.style.color = "red";
+    } else {
+        counter.style.color = "yellow";
+    }
 })
 
-increase.addEventListener("click",() => {
+increase.addEventListener("click", () => {
     count++;
     counter.textContent = count;
+    if (count > 0) {
+        counter.style.color = "green";
+    } else if (count < 0) {
+        counter.style.color = "red";
+    } else {
+        counter.style.color = "yellow";
+    }
 })
 
-if (counter > 0) {
-    counter.style.color = green;
-}
+
 //JavaScript program written by t3nsor
